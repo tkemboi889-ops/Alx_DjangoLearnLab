8aaabf7119a000c425a731cd34736694ec230c4d
timothy kemboi <tkemboi889@gmail.com>
2025-10-30 18:45:27 +0300

    add configurations to the admin interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from.models import Book
+from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  
     search_fields = ('title', 'author')  
