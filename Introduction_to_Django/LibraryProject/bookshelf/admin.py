203c6d8660778aae4d017061a8b0cc0c911c87ec
timothy kemboi <tkemboi889@gmail.com>
2025-10-30 18:36:10 +0300

    add initial commit of admin interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from.models import Book
+# Create a custom admin class
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # show these fields in list view
+    search_fields = ('title', 'author')  # allow searching by title or author
+    list_filter = ('publication_year',)  # add a filter by publication year
+
+# Register the model with the custom admin
+admin.site.register(Book, BookAdmin)
