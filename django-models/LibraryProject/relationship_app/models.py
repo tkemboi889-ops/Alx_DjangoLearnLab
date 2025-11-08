69930e768c297b3287fdd5389cfbb7af4bca2d81
timothy kemboi <tkemboi889@gmail.com>
2025-11-08 10:41:59 +0300

    add django models projects to the repo

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name=models.CharField(max_length=50)
+    def __str__(self):
+        return f"{self.name}"
+    
+class Book(models.Model):
+    title=models.CharField(max_length=100)
+    author=models.ForeignKey(Author,on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.title}"
+class Library(models.Model):
+    name=models.CharField(max_length=130)
+    books=models.ManyToManyField(Book)
+    def __str__(self):
+        return f"{self.name}"
+    
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
+
