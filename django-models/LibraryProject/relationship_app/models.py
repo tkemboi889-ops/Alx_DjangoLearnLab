24bc38dc24c765df1cfe3e6507c6f83b0344e185
timothy kemboi <tkemboi889@gmail.com>
2025-11-08 10:51:26 +0300

    add the django models to the repository

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,23 +4,24 @@ from django.db import models
 class Author(models.Model):
     name=models.CharField(max_length=50)
     def __str__(self):
-        return f"{self.name}"
+        return self.name
     
 class Book(models.Model):
     title=models.CharField(max_length=100)
     author=models.ForeignKey(Author,on_delete=models.CASCADE)
     def __str__(self):
-        return f"{self.title}"
+        return self.title
+    
 class Library(models.Model):
     name=models.CharField(max_length=130)
     books=models.ManyToManyField(Book)
     def __str__(self):
-        return f"{self.name}"
+        return self.name
     
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
 
