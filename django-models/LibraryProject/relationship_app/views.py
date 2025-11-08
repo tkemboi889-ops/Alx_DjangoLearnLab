73fe2b041007514fff99d8c9c41eef4a57ceb4cd
timothy kemboi <tkemboi889@gmail.com>
2025-11-08 14:56:03 +0300

    add django models project

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -31,3 +31,13 @@ class SignupView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration.html"
+
+from django.contrib.auth.views import LoginView
+
+class UserLoginView(LoginView):
+    template_name = 'login.html'
+
+from django.contrib.auth.views import LogoutView
+
+class UserLogoutView(LogoutView):
+    template_name = 'logout.html'
