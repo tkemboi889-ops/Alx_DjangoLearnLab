f890581c6d2c61793c1093912923cb236f400840
timothy kemboi <tkemboi889@gmail.com>
2025-11-08 13:02:25 +0300

    add django models to the repository

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'template/list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 from django.shortcuts import render
@@ -13,7 +13,7 @@ from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'template/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
 
 from django.shortcuts import render
 from django.views.generic import ListView
@@ -21,6 +21,6 @@ from .models import Library
 
 class LibrarylistView(ListView):
     model = Library
-    template_name = 'template/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     
 
