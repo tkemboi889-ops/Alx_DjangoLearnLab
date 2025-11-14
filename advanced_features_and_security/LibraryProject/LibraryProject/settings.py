c032faf11a049978c9fb8533d519640f2d28b36b
timothy kemboi <tkemboi889@gmail.com>
2025-11-14 13:01:21 +0300

    add advanced authentication features to reppo

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf.customuser',
+    'bookshelf.CustomUser',
     'relationship_app',
 ]
 
