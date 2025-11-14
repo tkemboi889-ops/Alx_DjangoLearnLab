10bab53bf7a2e30cf8245290ac01ec9001dad164
timothy kemboi <tkemboi889@gmail.com>
2025-11-14 12:58:56 +0300

    add advanced authentication features to repo

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 #customuser models
-AUTH_USER-MODEL='bookshelf.customuser'
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf',
+    'bookshelf.customuser',
     'relationship_app',
 ]
 
