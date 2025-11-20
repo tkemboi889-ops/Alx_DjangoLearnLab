2272cc42f76c92a427ef02b6c331634b8b929602
timothy kemboi <tkemboi889@gmail.com>
2025-11-20 11:26:24 +0300

    add API endpoint project to repository

--- api_project/api_project/api/views.py
+++ api_project/api_project/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+class Booklist(rest_framework.generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
