318d2c94a1ec9c6cd4af8060131a87e2e2fe7dcf
TIMOTHY KEMBOI <tkemboi889@gmail.com>
2025-11-20 09:30:27 +0300

    Delete api_project/api_project directory

--- api_project/api_project/api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Book
-# Register your models here.
-class Bookadmin(admin.ModelAdmin):
-    list_display=("title","author")
-admin.site.register(Book,Bookadmin)
