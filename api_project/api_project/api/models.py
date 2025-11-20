d54b839962603924d4e96e7cbe6db3e8818eba85
timothy kemboi <tkemboi889@gmail.com>
2025-11-20 10:21:05 +0300

    add api project to repository

--- api_project/api_project/api/models.py
+++ api_project/api_project/api/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Book(models.Model):
     title=models.CharField(max_length=100)
     author=models.CharField(max_length=150)
-    
+ #create another class
+class student(models.Model):
+    firstname=models.CharField(max_length=100) 
+    lastname=models.CharField(max_length=150)  
