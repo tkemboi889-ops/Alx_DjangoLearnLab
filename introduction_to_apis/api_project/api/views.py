c71e83559288117e2a1ae97c99ca7aaac1f9589d
timothy kemboi <tkemboi889@gmail.com>
2025-11-20 16:35:48 +0300

    add api implementation project

---
+++ introduction_to_apis/api_project/api/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+class Booklist(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
