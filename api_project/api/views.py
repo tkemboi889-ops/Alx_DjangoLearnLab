dc750f98aa7ea34fb327a9c49063fed017c770db
timothy kemboi <tkemboi889@gmail.com>
2025-11-20 17:35:13 +0300

    add implementation of apis

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,3 +5,6 @@ from .serializers import BookSerializer
 class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+class BookViewSet(rest_framework.viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
