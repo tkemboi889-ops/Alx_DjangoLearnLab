94870a6dc3e6d74788af9bbb4bd6ae94eecd7dac
timothy kemboi <tkemboi889@gmail.com>
2025-12-02 17:52:01 +0300

    add blog post management features

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,7 @@
 
 from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.contrib.auth.decorators import LoginRequiredMixin,UserPassesTestMixin
 from .forms import PostForm
 from .models import Post
 
